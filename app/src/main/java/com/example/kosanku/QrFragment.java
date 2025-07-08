e182f1dd1fa208dcfbabe27ae92c1e69f56064ef
NatanaelYG <natanaelyohanesg@gmail.com>
2025-07-08 15:12:08 +0700

    First Init

---
+++ app/src/main/java/com/example/kosanku/QrFragment.java
@@ -0,0 +1,18 @@
+package com.example.kosanku;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class QrFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.qr_fragment, container, false);
+    }
+
+}
